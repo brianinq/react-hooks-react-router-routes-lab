423a6489d6aa2ffc370844d6e49a93e0940c8920
brian <kipkoechbrian0@gmail.com>
2022-09-28 22:09:39 +0300

    copmlete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,17 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path={"/"} exact element={<Home />} />
+        <Route path={"/movies"} exact element={<Movies />} />
+        <Route path={"/directors"} exact element={<Directors />} />
+        <Route path={"/actors"} exact element={<Actors />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, i) => {
+        return (
+          <div key={i}>
+            <p>{movie.title}</p>
+            <small>{movie.time}</small>
+            <ul>
+              {movie.genres.map((genre, y) => (
+                <li key={y}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
